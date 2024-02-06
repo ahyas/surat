c99d77bff94eea5efa061ed1f2248d246b41441b
ahyasw <ahyasw@gmail.com>
2024-02-07 04:34:48 +0900

    Adding Ilustration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <!--begin::Post-->
+
 <div class="content flex-column-fluid" id="kt_content">
     <div class="card h-100" >
         <div class="card-header border-0 pt-6">
@@ -16,8 +17,11 @@
                 
                 <span class="text-primary">{{Auth::user()->name}}</span>
             </h4>
-            <h4 class="text-gray-600">Di Aplikasi SIMANSUR</h4>
+            <h4 style="color:#C62828">Di Aplikasi SIMANSUR</h4>
             <h4 class="text-gray-600">(Sistem Informasi Manajemen Persuratan)</h4>
+            <div style="display:grid">
+                <img src="{{asset('public/assets/media/illustrations/dozzy-1/16.png')}}" style="position:absolute; bottom:0px; right:0px; width:500px; margin-bottom:25px;" />
+            </div>
         </div>
     </div>
 
